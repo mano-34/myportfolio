dd9092c2e56462408cd1d0387861f820621d4423
TJ-mano <manojkumar@techjose.com>
2025-11-08 12:25:42 +0530

    Update React app with latest changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,21 @@
 import React from "react";
-
+import { FaLinkedin, FaEnvelope, FaGithub } from "react-icons/fa6";
 
 export default function Footer() {
   return (
     <footer className="footer">
       © {new Date().getFullYear()} Mano | All Rights Reserved
+       <div className="footer-socials">
+                <a href="https://linkedin.com/in/mano305" target="_blank" rel="noreferrer">
+                  <FaLinkedin size={20} />
+                </a>
+                <a href="mailto:manojkumar15112004@email.com" target="_blank" rel="noreferrer">
+                  <FaEnvelope size={20} />
+                </a>
+                <a href="https://github.com/mano-34" target="_blank" rel="noreferrer">
+                  <FaGithub size={20} />
+                </a>
+              </div>
     </footer>
   );
 }
